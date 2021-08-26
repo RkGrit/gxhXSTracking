8ae16395e42ecf8d772c6f7673cfb1be951d25c9
RkGrit <G1220710602@gmail.com>
2021-08-26 13:21:19 +0800

    guxinhao's first commit

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,31 +1,11 @@
-//
-// Created by li on 22/8/2021.
-//
+/* 
+Created by li on 22/8/2021.
+Modified by Gu on 24/8/2021
+*/
 #include "camera.h"
 
-
-UVC::UVC(int cam) {
-    cam_type_ = CAMERA_TYPE_UVC;
-    fb_ = cv::VideoCapture(cam);
-    if(fb_.isOpened()){
-        cam_id_=cam_count_++;
-    }
-}
-
-UVC::UVC(std::string path) {
-    cam_type_ = CAMERA_TYPE_UVC;
-    fb_ = cv::VideoCapture(path);
-    if(fb_.isOpened()){
-        cam_id_=cam_count_++;
-    }
-}
-
-bool UVC::GetImg(Ximg &img) {
-    cv::Mat src;
-    fb_ >> src;
-    img = Ximg(this,std::move(src));
-    return true;
-}
+int UVC::getUVCTimeNums = 0;
+int XSCAM::getXSTimeNums = 0;
 
 std::shared_ptr<Camera> CameraFactory::CreateCamera(CAMERA_TYPE cam_type, std::string path) {
     switch(cam_type){
@@ -40,20 +20,48 @@ std::shared_ptr<Camera> CameraFactory::CreateCamera(CAMERA_TYPE cam_type, std::s
     }
 }
 
-std::shared_ptr<Camera> CameraFactory::CreateCamera(CAMERA_TYPE cam_type, int id) {
-    switch(cam_type){
-        case CAMERA_TYPE_UVC:
-            return std::make_shared<UVC>(id);
-            break;
-        default:
-            std::cerr<<"camera type invalid!"<<std::endl;
-    }
+UVC::UVC(int cam) {
+    cam_type_ = CAMERA_TYPE_UVC;
+    fb_ = cv::VideoCapture(cam);
 }
 
-XSCAM::XSCAM(std::string path) {
+UVC::UVC(std::string path) {
+    cam_type_ = CAMERA_TYPE_UVC;
+    this->path_=path;
+    fb_ = cv::VideoCapture(path);
+    GetTimestampFromSei(path,UVC_TV);
+    totalTimeNUms = UVC_TV.size();
+}
 
+bool UVC::GetImg(Ximg &img) {
+    cv::Mat src;
+    fb_ >> src;
+    XSTime Img_time = this->UVC_TV[getUVCTimeNums];
+    //img = Ximg(this,std::move(src));
+    img = Ximg(this, std::move(src), Img_time,getUVCTimeNums);
+    getUVCTimeNums++;
+    return true;
+}
+
+
+
+XSCAM::XSCAM(std::string path) {
+    cam_type_ = CAMERA_TYPE_XSCAM;
+    this->path_=path;
+    rtspVC = cv::VideoCapture(path);
+    GetTimestampFromSei(path,XSCAM_TV);
+    totalTimeNUms = XSCAM_TV.size();
 }
 
 bool XSCAM::GetImg(Ximg &img) {
-
+    cv::Mat src;
+    rtspVC >> src;
+    XSTime Img_time = this->XSCAM_TV[getXSTimeNums];
+    //img = Ximg(this,std::move(src));
+    img = Ximg(this, std::move(src), Img_time,getXSTimeNums);
+    getXSTimeNums++;
+    return true;
 }
+
+
+
--- src/camera.h
+++ src/camera.h
@@ -1,6 +1,7 @@
-//
-// Created by li on 22/8/2021.
-//
+/* 
+Created by li on 22/8/2021.
+Modified by Gu on 24/8/2021
+ */
 
 #ifndef XSTRACKING_CAMERA_H
 #define XSTRACKING_CAMERA_H
@@ -10,7 +11,6 @@ class CameraFactory{
 public:
     CameraFactory()=default;
     static std::shared_ptr<Camera> CreateCamera(CAMERA_TYPE cam_type, std::string path);
-    static std::shared_ptr<Camera> CreateCamera(CAMERA_TYPE cam_type, int id);
 };
 
 class UVC: public Camera{
@@ -19,8 +19,14 @@ public:
     UVC(std::string path);
     bool GetImg(Ximg &img);
 
+    static int getUVCTimeNums; //静态成员，记录当前推送了几帧。
+
 protected:
     cv::VideoCapture fb_;
+    std::string path_; //视频地址
+    std::vector<XSTime> UVC_TV; //保存时间戳的动态数组
+    int totalTimeNUms; //一共有多少帧的时间戳
+    
 };
 
 class XSCAM: public Camera{
@@ -28,7 +34,13 @@ public:
     XSCAM(std::string path);
     bool GetImg(Ximg &img);
 
+    static int getXSTimeNums; //静态成员，记录当前推送了几帧。
+
 protected:
+    cv::VideoCapture rtspVC;
+    std::string path_; //视频地址
+    std::vector<XSTime> XSCAM_TV; //保存时间戳的动态数组
+    int totalTimeNUms; //一共有多少帧的时间戳
 };
 
 
